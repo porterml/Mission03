110fc917ad31572e2244d123c8a651dfb91b722c
porterml <90496293+porterml@users.noreply.github.com>
2022-01-19 21:23:56 -0700

    Turn in

---
+++ MyFirstASPNETWebApp/Controllers/BlahController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstASPNETWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstASPNETWebApp.Controllers
+{
+    public class BlahController : Controller
+    {
+        public IActionResult Index ()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Calculator ()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calculator (CalculatorModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ MyFirstASPNETWebApp/Models/ControllerModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstASPNETWebApp.Models
+{
+    public class CalculatorModel
+    {
+        [Range(0,100)]
+        public int Assignment { get; set; }
+        [Range(0, 100)]
+        public int Quiz { get; set; }
+        [Range(0, 100)]
+        public int Intex { get; set; }
+        [Range(0, 100)]
+        public int Project { get; set; }
+        [Range(0, 100)]
+        public int Exam { get; set; }
+    }
+}
